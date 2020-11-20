030f6359d5780c03d817f5fbecc924117b2c07e5
nightskin <angelastray@hotmail.com>
2020-11-19 19:44:45 -0500

    Initial commit

---
+++ Assets/_Scripts/LOS.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LOS : MonoBehaviour
+{
+    public Collider2D collidesWith;
+    public List<Collider2D> colliders;
+    public ContactFilter2D contactFilter;
+
+    private Collider2D LOSCollider;
+
+    void Start()
+    {
+        LOSCollider = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Physics2D.GetContacts(LOSCollider,contactFilter, colliders);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        collidesWith = other;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# GAME2014-F2020-Lesson11
+
+Demo Project for GAME2014 - Lesson 11
